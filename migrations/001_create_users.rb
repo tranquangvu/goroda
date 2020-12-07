a2ab83e971923c8d2a0139c68425519abf7dbc8f
tranquangvu <vutran.513@gmail.com>
2020-12-07 10:37:10 +0700

    Add users model

---
+++ migrations/001_create_users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table(:users) do
+      primary_key :id, unique: true
+      String :name, null: false
+      String :email, unique: true, null: false
+      String :password_hash, null: false
+    end
+  end
+end
